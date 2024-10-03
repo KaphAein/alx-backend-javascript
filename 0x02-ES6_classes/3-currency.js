31e8fc3db0f51c32b75c077aea5a9a8d59a6a28c
KaphAein <cherifafadaly@gmail.com>
2024-10-03 13:44:33 +0000

    commit

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,50 +1,27 @@
-/**
- * Represents a Holberton Course.
- */
 export default class Currency {
-  /**
-   * Creates a new @see {@link Currency}.
-   *
-   * @param {String} code - code.
-   * @param {String} name - name.
-   */
+
   constructor(code, name) {
     this.code = code;
     this.name = name;
   }
 
-  /**
-   * Gets the code.
-   */
   get code() {
     return this._code;
   }
 
-  /**
-   * Sets the code.
-   */
   set code(value) {
     this._code = value;
   }
 
-  /**
-   * Gets the name.
-   */
   get name() {
     return this._name;
   }
 
-  /**
-   * Sets the name.
-   */
   set name(value) {
     this._name = value;
   }
 
-  /**
-   * Creates the full string representation of this Currency.
-   * @returns {String}
-   */
   displayFullCurrency() {
     return `${this._name} (${this._code})`
+  }
 }
