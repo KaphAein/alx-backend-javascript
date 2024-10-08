5c987bba5fc41c25bdb62c86804276e33cd13f75
KaphAein <cherifafadaly@gmail.com>
2024-10-08 12:28:23 +0000

    commit

---
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -0,0 +1,7 @@
+export default function getStudentIdsSum(students) {
+  if (students instanceof Array) {
+    return students.reduce(
+      (total, student) => total + student.id, 0);
+  }
+  return [];
+}
