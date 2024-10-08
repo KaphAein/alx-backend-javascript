ad20b67063deda8eaf21747bd7a83c39fe2089be
KaphAein <cherifafadaly@gmail.com>
2024-10-08 12:06:04 +0000

    commit

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,13 +1,6 @@
-/**
- * Retrieves a list of students.
- * @author Cherif Fadaly
- * @returns array of ids from a list of object
- */
-export default function getListStudentsIds() {
-  const students = [
-    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-    { id: 2, firstName: 'James', location: 'Columbia' },
-    { id: 5, firstName: 'Serena', location: 'San Francisco' },
-  ];
-  return students;
+export default function getListStudentsIds(students) {
+  if (students instanceof Array) {
+    return students.map(student => student.id);
+  }
+  return []
 }
