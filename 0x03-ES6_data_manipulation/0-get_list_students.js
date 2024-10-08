68e3c149567d08ff08653560dc6de05e62b436f1
KaphAein <cherifafadaly@gmail.com>
2024-10-08 10:59:51 +0000

    commit

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,13 @@
+/**
+ * Retrieves a list of students.
+ * @author Cherif Fadaly
+ * @returns {{id: Number, firstName: String, location: String}[]}
+ */
+export default function getListStudents() {
+  const students = [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' }
+  ];
+  return (students);
+}
