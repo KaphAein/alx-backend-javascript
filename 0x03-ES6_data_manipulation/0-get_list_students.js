9138914bad957238e6f017d3e7a20ae72bf6f24b
KaphAein <cherifafadaly@gmail.com>
2024-10-08 11:36:28 +0000

    commit

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,8 +1,12 @@
+/**
+ * Retrieves a list of students.
+ * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ * @returns {{id: Number, firstName: String, location: String}[]}
+ */
 export default function getListStudents() {
-  const students = [
+  return [
     { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
-    { id: 5, firstName: 'Serena', location: 'San Francisco' }
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
   ];
-  return students;
 }
